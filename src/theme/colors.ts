5be2d58f9e205d24f7ef1dafbf39db2eaff64878
Raushan Kumar <raushankumar6511@gmail.com>
2024-09-26 10:43:58 +0530

    changed splash screen

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,7 +2,9 @@ import {palette} from './palette';
 
 export const colors = {
   palette,
-  primary: '#000000',
+  // primary: '#000000',
+  primary: '#276a72',
+  // primary: '#008080',
   secondary: '#f3f3f3',
   background: '#FAFBFF',
   transparent: 'rgba(0, 0, 0, 0.3)',
